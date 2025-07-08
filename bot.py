5b944757ddba5900be5a6c578c24b20ec049e951
justin70809 <ahsomeone607@gmail.com>
2025-07-08 21:19:25 +0800

    1

--- bot.py
+++ bot.py
@@ -355,7 +355,7 @@ async def on_message(message):
 
                 response = client_ai.responses.create(
                     model=model_used,  # 使用動態決定的模型
-                    tools={"type": "web_search_preview"},
+                    tools=[{"type": "web_search_preview"}],
                     input=input_prompt,
                     previous_response_id=state["last_response_id"],
                     store=True
